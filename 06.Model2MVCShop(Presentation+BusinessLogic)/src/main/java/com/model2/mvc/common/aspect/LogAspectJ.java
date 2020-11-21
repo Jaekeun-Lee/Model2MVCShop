df7e7b1c7068765b1be6ebee3c6daf055719054d
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-11-21 13:53:23 +0900

    Product part integrate

--- 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/common/aspect/LogAspectJ.java
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/common/aspect/LogAspectJ.java
@@ -1,19 +1,17 @@
 package com.model2.mvc.common.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 
-/*
- * FileName : PojoAspectJ.java
- *	:: XML 에 선언적으로 aspect 의 적용   
-  */
+@Aspect
 public class LogAspectJ {
 
-	///Constructor
 	public LogAspectJ() {
 		System.out.println("\nCommon :: "+this.getClass()+"\n");
 	}
 	
-	//Around  Advice
+	@Around("execution(* com.model2.mvc.service..*Impl.*(..) )")
 	public Object invoke(ProceedingJoinPoint joinPoint) throws Throwable {
 			
 		System.out.println("");
